c5da44fd9828566f94f2c06e3ee34b31248775aa
leoliver <leosdoliver@gmail.com>
2025-11-03 20:45:48 -0300

    Configuração do Banco de dados Supabase

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from src.config.data_base import init_db, db
 from src.routes import init_routes
-from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
+from flask_jwt_extended import JWTManager
+import os
 
 def create_app():
     """
@@ -9,18 +11,22 @@ def create_app():
     """
     app = Flask(__name__)
 
-    init_db(app)
+    CORS(app, origins=['https://react-gestao-de-estoque.vercel.app/'])
 
+    init_db(app)
     init_routes(app)
+
+    app.config["JWT_SECRET_KEY"] = "e999c435-6b35-4c11-89f3-efe62dd15f08"
 
     return app
 
 app = create_app()
-app.config["JWT_SECRET_KEY"] = "e999c435-6b35-4c11-89f3-efe62dd15f08"
 jwt = JWTManager(app)
+
 
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)), debug=True)
--- src/config/data_base.py
+++ src/config/data_base.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
-import pymysql
 
 db = SQLAlchemy()
 
@@ -8,23 +7,31 @@ def init_db(app):
     """
     Inicializa a base de dados com o app Flask e o SQLAlchemy.
     """
-    db_user = 'root'
-    db_password = 'root'
-    db_name = 'sellerdatabase'
-    db_host = 'localhost'
-    db_port = 3306
+    # db_user = 'root'
+    # db_password = 'root'
+    # db_name = 'sellerdatabase'
+    # db_host = 'localhost'
+    # db_port = 3306
 
-    conn = pymysql.connect(host=db_host, user=db_user, password=db_password, port=db_port)
-    cursor = conn.cursor()
-    cursor.execute(
-        f"CREATE DATABASE IF NOT EXISTS {db_name} "
-        "CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-    )
-    conn.commit()
-    cursor.close()
-    conn.close()
+    # conn = pymysql.connect(host=db_host, user=db_user, password=db_password, port=db_port)
+    # cursor = conn.cursor()
+    # cursor.execute(
+    #     f"CREATE DATABASE IF NOT EXISTS {db_name} "
+    #     "CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    # )
+    # conn.commit()
+    # cursor.close()
+    # conn.close()
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_user}:{db_password}@localhost/{db_name}'
+    database_url = os.getenv("DATABASE_URL")
+
+    if not database_url:
+        raise ValueError("Variável de ambiente DATABASE_URL não definida.")
+    
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql+psycopg2://", 1)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
