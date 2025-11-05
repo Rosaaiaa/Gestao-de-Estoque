c1306702f0ae4df00a8d04bed5595cf91f9bc9d4
leoliver <leosdoliver@gmail.com>
2025-11-05 20:02:08 -0300

    Ajuste configuração Twilio

--- src/Infrastructure/http/whats_app.py
+++ src/Infrastructure/http/whats_app.py
@@ -1,19 +1,20 @@
 from twilio.rest import Client
 import random
+import os
 
 def gera_codigo():
-  numero_aleatorio = random.randint(1000, 9999)
+    numero_aleatorio = random.randint(1000, 9999)
 
-  account_sid = ''
-  auth_token = ''
-  client = Client(account_sid, auth_token)
+    account_sid = f'{os.getenv("TWILIO_ACCOUNT_ID")}'
+    auth_token = f'{os.getenv("TWILIO_AUTH_TOKEN")}'
+    client = Client(account_sid, auth_token)
 
-  message = client.messages.create(
-    from_='whatsapp:+14155238886',
-    content_sid='HX229f5a04fd0510ce1b071852155d3e75',
-    content_variables='{"1":"'+ str(numero_aleatorio) +'"}',
-    to='whatsapp:+5511995634027'
-  )
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        content_sid='HXb5b62575e6e4ff6129ad7c8efe1f983e',
+        content_variables='{"1":"12/1","2":"3pm"}',
+        to='whatsapp:+5511985528776'
+    )
 
-  message.sid
-  return numero_aleatorio
+    message.sid
+    return numero_aleatorio
